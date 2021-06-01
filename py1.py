9602a51b8465f9116ad87b6010cc95fe716876eb
Harsha266 <harshaharshi152@gmail.com>
2021-06-01 14:13:51 +0530

    HOpe so

--- py1.py
+++ py1.py
@@ -3,7 +3,7 @@ if p :
     print("Eat fast")
 else:
     print("Eat late")
-print("helo  everyone this is there")
+print("helo everyone this is there")
 print("Hoi bro")
 
 print("this is the new feature to be added")
