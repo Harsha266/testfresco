f9d7f69a7707bfe6eff5ab0a3dde2e61b3437893
Harsha266 <harshaharshi152@gmail.com>
2021-06-01 13:06:49 +0530

    feature new one

--- py1.py
+++ py1.py
@@ -3,7 +3,8 @@ if p :
     print("Eat fast")
 else:
     print("Eat late")
-print("helo everyone this is there")
+print("heloeveryone this is there")
 print("Hoi bro")
 
 print("this is the new feature to be added")
+print('this is another line for the new feature branch")
