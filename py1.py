817073ff405a8010f5b8170535721ba2ddbbbdbe
Harsha266 <harshaharshi152@gmail.com>
2021-06-01 13:33:59 +0530

    Hope merge conflists doenst occur

--- py1.py
+++ py1.py
@@ -3,8 +3,10 @@ if p :
     print("Eat fast")
 else:
     print("Eat late")
-print("heloeveryone this is there")
+print("helo  everyone this is there")
 print("Hoi bro")
 
 print("this is the new feature to be added")
 print("this is another line for the new feature branch")
+
+print("Hope so the merge conflits doesn't arrsises")
